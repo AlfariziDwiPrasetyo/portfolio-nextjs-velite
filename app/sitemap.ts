1e45c268bcde5cd5c8a99d27f227d0f8ebca2117
Al Farizi Dwi Prasetyo <yoerdanatan@gmail.com>
2024-10-10 13:35:17 +0700

    chore : change url sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,19 +3,19 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://shirookami.vercel.app",
+      url: "https://shirookami.my.id",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
     },
     {
-      url: "https://shirookami.vercel.app/work",
+      url: "https://shirookami.my.id/work",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.8,
     },
     {
-      url: "https://shirookami.vercel.app/writing",
+      url: "https://shirookami.my.id/writing",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.5,
